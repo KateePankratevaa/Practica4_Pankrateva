e2f24ef90d8ff0f4e6ef956fe7e427b37f32b93f
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2024-02-10 00:33:42 +0500

    Add files via upload

---
+++ Pract4_Pankrateva/Pract4_Pankrateva/Pract4_Pankrateva.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    ifstream file("Bagazh.txt");
+
+    if (!file.is_open()) {
+        cout << "Ошибка открытия файла!" << endl;
+        return 1;
+    }
+
+    cout << "Пассажиры, имеющие более двух вещей в багаже:\n" << endl;
+
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
+        int numItems;
+        double weight;
+        iss >> numItems >> weight;
+
+        if (numItems > 2) {
+          
+            cout << "Количество вещей: " << numItems << " Вес в килограммах " << weight << endl;
+        }
+    }
+
+    file.close();
+    return 0;
+}
